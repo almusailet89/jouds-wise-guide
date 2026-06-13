87a7719ad8274f2bc0db4c99108ef966e42f0c75
Abdulrhaman Adel A Almusailet <Almusailet@icloud.com>
2026-06-13 14:28:03 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/jouds-wise-guide/sw.js', { scope: '/jouds-wise-guide/' })})}
